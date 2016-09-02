c9f3b8235544b3451bd56f4cbe90bb4f5a56c1a4
Jorge Mero <Jorge Mero@192.168.0.3>
2016-09-02 06:56:14 -0500

    2 commit

--- PruebaIng/src/pruebaing/PruebaIng.java
+++ PruebaIng/src/pruebaing/PruebaIng.java
@@ -17,7 +17,7 @@ public class PruebaIng {
     public static void main(String[] args) {
         // TODO code application logic here
     
-        System.out.println("Hola q tal");
+        System.out.println("Mi nombre es");
     }
     
 }
